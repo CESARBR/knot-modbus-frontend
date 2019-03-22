140f5631ba483af6fa8f350ffbe63d2339e05ed8
Ramon Henrique <rhpr@cesar.org.br>
2019-03-21 23:26:49 -0300

    Add initial support to slave service
    This patch add placeholder functions in an abstract layer to allow
    multprotocols to backend

---
+++ server/SlaveService/index.js
@@ -0,0 +1,22 @@
+class SlaveService {
+  constructor(protocol, config) { // eslint-disable-line no-unused-vars
+    switch (protocol) {
+      case 'DBUS':
+        // init service
+        break;
+      default:
+        break;
+    }
+  }
+
+  execute() { // eslint-disable-line class-methods-use-this
+  }
+
+  list() { // eslint-disable-line class-methods-use-this
+  }
+
+  get(id) { // eslint-disable-line no-unused-vars, class-methods-use-this
+  }
+}
+
+export default SlaveService;
