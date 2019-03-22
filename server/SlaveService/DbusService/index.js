2d1c36c28be96aab4c26a65d7154014d0f594b84
Ramon Henrique <rhpr@cesar.org.br>
2019-03-21 23:51:52 -0300

    Add initial dbus service

---
+++ server/SlaveService/DbusService/index.js
@@ -0,0 +1,15 @@
+class DbusServices {
+  constructor(config) { // eslint-disable-line no-unused-vars, no-useless-constructor
+  }
+
+  execute() { // eslint-disable-line class-methods-use-this
+  }
+
+  list() { // eslint-disable-line class-methods-use-this
+  }
+
+  get(id) { // eslint-disable-line no-unused-vars, class-methods-use-this
+  }
+}
+
+export default DbusServices;
