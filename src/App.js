90aba7360ae41c922f58c3b4d1db11a5a634a2f8
Ramon Ribeiro <rhpr@cesar.org.br>
2019-04-01 11:28:36 -0300

    Show snackbar when generate error in list devices

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
 import SlaveService from './services/Slave';
 import SlaveCard from './components/SlaveCard';
 
@@ -9,15 +10,21 @@ class App extends Component {
     super(props);
     this.state = {
       renderCard: false,
-      slaves: []
+      slaves: [],
+      openSnack: false,
+      messageSnack: ''
     };
   }
 
   onClicked(slaveSrv) {
-    slaveSrv.listSlaves().then((slaves) => {
-      this.setState({ renderCard: true });
-      this.setState({ slaves: slaves.data });
-    });
+    slaveSrv.listSlaves()
+      .then((slaves) => {
+        this.setState({ renderCard: true });
+        this.setState({ slaves: slaves.data });
+      })
+      .catch((err) => {
+        this.setState({ openSnack: true, messageSnack: err.response.data });
+      });
   }
 
   renderCardSlaves() {
@@ -27,12 +34,22 @@ class App extends Component {
 
   render() {
     const slaveSrv = new SlaveService();
-    const { renderCard } = this.state;
+    const { renderCard, openSnack, messageSnack } = this.state;
     return (
       <div className="App">
         <Button className="load-button" onClick={() => this.onClicked(slaveSrv)} variant="contained" color="primary">
           Load slaves
         </Button>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          open={openSnack}
+          autoHideDuration={3000}
+          onClose={() => this.setState({ openSnack: false })}
+          message={messageSnack}
+        />
 
         { renderCard === true ? this.renderCardSlaves() : null }
       </div>
